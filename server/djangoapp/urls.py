7b6b0933889a7dd5315bb1bdd526ec29405b4a92
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-05-29 10:45:44 -0400

    Fixed urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    path(route='logout', views=views.logout_request, name='logout')
+    path(route='logout', view=views.logout_request, name='logout')
 
     # path for dealer reviews view
 
