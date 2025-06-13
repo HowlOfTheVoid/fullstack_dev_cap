e7d2549c4e160f69587b031d52b193407d49ade7
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-06-13 11:14:15 -0400

    Fixed filtering, work on Dealer by ID

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,15 +98,15 @@ def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers"+state
+        endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
-        reviews = get_reuqest(endpoint)
+        reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
@@ -121,6 +121,7 @@ def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealers/" + str(dealer_id)
         dealership = get_request(endpoint)
+        print(dealership)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
