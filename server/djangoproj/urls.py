96616b9de9624347366e667154ca391ac960268d
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-06-16 11:16:56 -0400

    Began working on Dealer.jsx issues

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
