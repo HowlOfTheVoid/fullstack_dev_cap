e3cb2756f79c4c586c0180bd88aef00d0e45fb57
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-06-20 12:53:04 -0400

    Enabled containerized deployment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,10 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register',
+         view=views.registration,
+         name='register'),
+    
     # path for login
     path(route='login',
          view=views.login_user,
